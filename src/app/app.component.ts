d9d446ac37a07dbe734347942d7f8d0c0570364d
PatrickJS <github@gdi2290.com>
2023-05-30 15:20:10 -0700

    1 file modified, 1 added

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Hello from {{name}}!</h1>
+    <a target="_blank" href="https://angular.io/start">
+      Learn more about Angular 
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
