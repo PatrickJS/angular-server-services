6d50fb59c5605425bb421cb643ef55644154fcb6
PatrickJS <github@gdi2290.com>
2023-05-30 22:14:19 -0700

    update

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,7 +2,7 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 
-import { ExampleService } from '@server/Example.service';
+import { ExampleService } from '@server/ExampleService';
 
 
 const serverConfig: ApplicationConfig = {
